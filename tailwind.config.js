98af8edc2f2a70eca2f96d8f57006aace9d03a0d
hossein haji alizadeh <hosseinlight84@gmail.com>
2022-11-29 03:35:37 +0330

    completed header but without currect drop downmenu

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: { 
+      spacing: {
+        '4.5': '20px',
+      }
+      
+    },
+  },
+  plugins: [],
+}
